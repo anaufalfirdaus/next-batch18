cc711644aba30b98996b696e366a03f0d8660f5f
naufalfirdaus <naufalfirdausa@gmail.com>
2022-11-01 09:50:00 +0700

    day02

--- pages/Region/RegionView.js
+++ pages/Region/RegionView.js
@@ -5,8 +5,6 @@ import { DelRegionRequest, GetRegionRequest } from '../redux-saga/Action/RegionA
 import { Menu, Transition } from '@headlessui/react'
 import FormikAddRegionApi from './FormikAddRegionApi';
 import FormikEditRegionApi from './FormikEditRegionApi';
-import { domain, image, urlImg } from '../config/config';
-//theming toast
 import Link from 'next/link';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -14,19 +12,11 @@ import Header from '../component/Header/Header';
 import Layout from '../component/layout/Layout'
 import {
     DotsVerticalIcon,
-    DuplicateIcon,
-    PhotographIcon,
     PencilAltIcon,
     TrashIcon,
-    UserAddIcon,
-    LockClosedIcon,
-    RefreshIcon,
     ChevronRightIcon,
     ChevronLeftIcon
 } from '@heroicons/react/solid'
-
-import { useRouter } from 'next/router';
-import Image from 'next/image';
 
 const columns = [
     { name: 'REGION NAME' },
@@ -41,7 +31,6 @@ function classNames(...classes) {
 
 export default function Batch() {
     const dispatch = useDispatch();
-    const router = useRouter();
     const [listRegion, setListRegion] = useState([])
     const [pageNumbers, setPageNumbers] = useState([])
     const [currentPage, setCurrentPage] = useState(1)
@@ -53,7 +42,6 @@ export default function Batch() {
 
     //1.create state batches
     const { regions } = useSelector((state) => state.regionStated)
-    const { UserProfile } = useSelector((state) => state.usrStated);
     const [filter, setFilter] = useState({
         input: ''
     })
@@ -79,8 +67,8 @@ export default function Batch() {
         setFilter({ ...filter, [name]: event.target.value });
     };
 
-    const onSearch = event => {
-        event.preventDefault();
+    const onSearch = (e) => {
+        e.preventDefault();
         setListRegion(
             Array.isArray(regions) && regions.filter(data => (
                 (data.regionName.toLowerCase().includes(filter.input.toLowerCase()))))
@@ -128,7 +116,7 @@ export default function Batch() {
                                                 className="form-control relative w-48 block px-2 py-0.5 text-xs font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:border-transparent focus:text-gray-700 focus:ring-1 focus:ring-offset-1 focus:ring-purple-500 focus:outline-none" placeholder="region name" aria-label="Search" aria-describedby="button-addon2" />
                                             <button
                                                 type="submit"
-                                                onClick={onSearch}
+                                                onClick={(e)=>onSearch(e)}
                                                 className="btn px-3 py-1 bg-orange-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-purple-500 transition duration-150 ease-in-out flex items-center" id="button-addon2">
                                                 <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="search" className="w-3" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                                                     <path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
@@ -156,8 +144,7 @@ export default function Batch() {
                                                     <tr key={data.regionId}>
                                                         <td className="px-6 py-2 text-center whitespace-nowrap text-sm text-gray-900">{data.regionName}</td>
                                                         <td className="px-6 py-2 text-center whitespace-nowrap text-sm text-gray-900">{data.regionFile}</td>
-                                                        <td className="px-6 py-2 text-center whitespace-nowrap text-sm text-gray-900"><img src={data.regionPhoto?`${urlImg}/${data.regionPhoto}`:"/favicon.ico"} width={20} height={10} alt="profile"/></td>
-                                                        {/* <td className="px-6 py-2 text-center whitespace-nowrap text-xs text-gray-900"><Image src={data.regionPhoto?`${domain}/api/region/files/${data.regionPhoto}`:"/favicon.ico"} width={20} height={10} alt='profile'/></td> */}
+                                                        <td className="px-6 py-2 text-center whitespace-nowrap text-sm text-gray-900">{data.regionPhoto}</td>
                                                         <td className="pr-6">
                                                             <Menu as="div" className="relative flex justify-end items-center">
                                                                 {({ open }) => (
